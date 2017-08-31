8d2bedb8977004cfd7abdd134b91f40910cb82ae
Pramod Rao <spramod49@gmail.com>
2017-08-31 23:30:29 +0530

    First version complete along with contact us form back-end.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $(function() {
   var navHeight = $(".navbar").outerHeight();
-  $('a[href*="#"]:not([href="#"])').click(function() {
+  $('a[href*="#"]:not([href*="Tab"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html, body').animate({
           scrollTop: target.offset().top - navHeight
-        }, 1000);
+      }, 700);
         return false;
       }
     }
